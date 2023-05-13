d04af55e60a4274b8534da9de58a9c14d057b1bd
Harsha Vardhan <gvsharsha6316@gmail.com>
2023-05-13 11:49:08 -0400

    added parameter form console

--- src/test/java/MyTestCases/LoginTest.java
+++ src/test/java/MyTestCases/LoginTest.java
@@ -3,21 +3,26 @@ package MyTestCases;
 import org.testng.annotations.Test;
 
 public class LoginTest {
+    String b = System.getProperty("browser");
 
     @Test
     public void testFirst() {
         System.out.println("hi this my first test");
+
+        System.out.println("broswer name selected: "+ b);
     }
 
     @Test
     public void testSecond() {
         System.out.println("hi this my second test");
         System.out.println("this parameter is coming from console: "+System.getProperty("name"));
+        System.out.println("broswer name selected: "+ b);
     }
 
     @Test
     public void testThird() {
         System.out.println("hi this my Third test");
+        System.out.println("broswer name selected: "+ b);
     }
 
 }
